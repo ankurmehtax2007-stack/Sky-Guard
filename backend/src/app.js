dccfebfe565b3fbcd258865ba3c0a8069d5b9be1
priyanshu vats <priyanshuvats@priyanshus-Laptop.local>
2026-08-27 20:51:30 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(pinoHttp({ logger }));
 
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
